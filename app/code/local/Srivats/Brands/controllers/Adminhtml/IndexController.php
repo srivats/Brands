167084cca2fd38ae3a2e00e427b76250f0839219
Srivats <blackcaps16@gmail.com>
2012-01-22 06:15:49 +0530

    first commit

---
+++ app/code/local/Srivats/Brands/controllers/Adminhtml/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Srivats_Brands_Adminhtml_IndexController extends Mage_Adminhtml_Controller_Action
+{
+    public function loadAction()
+    {
+        echo "Admin loaded";
+    }
+}
---
+++ app/code/local/Srivats/Brands/controllers/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Srivats_Brands_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        echo "index loaded";
+    }
+}
